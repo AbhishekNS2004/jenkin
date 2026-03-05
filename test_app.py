7add5cf9c4550ea41f281e49ce5557fc98b1e853
AbhishekNS2004 <abhishekns142@gmail.com>
2026-03-05 23:44:36 +0530

    se

--- test_app.py
+++ test_app.py
@@ -1,12 +1,18 @@
-import pytest
-from app import app
+import unittest
+from app import reverse_string
 
-@pytest.fixture
-def client():
-    app.testing = True
-    return app.test_client()
 
-def test_home(client):
-    response = client.get("/")
-    assert response.status_code == 200
-    assert b"Hello Jenkins CI/CD" in response.data
+class TestReverseString(unittest.TestCase):
+
+    def test_simple(self):
+        self.assertEqual(reverse_string("hello"), "olleh")
+
+    def test_empty(self):
+        self.assertEqual(reverse_string(""), "")
+
+    def test_numbers(self):
+        self.assertEqual(reverse_string("12345"), "54321")
+
+
+if __name__ == "__main__":
+    unittest.main()
